824a900567565a8259383be7c57876b8a2cc0a21
David Quast <dquast@ryerson.ca>
2019-01-15 10:33:37 -0500

    test(app): Added basic tests to allow publish to run.

---
+++ src/components/map-editor/map-editor.spec.ts
@@ -0,0 +1,7 @@
+import { RLMapEditor } from './map-editor';
+
+describe('map-editor', () => {
+  it('builds', () => {
+    expect(new RLMapEditor()).toBeTruthy();
+  });
+});
---
+++ src/components/map/map.spec.ts
@@ -0,0 +1,7 @@
+import { RLMap } from './map';
+
+describe('map-editor', () => {
+  it('builds', () => {
+    expect(new RLMap()).toBeTruthy();
+  });
+});
