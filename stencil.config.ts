4cbbb7cd313ae688f6db435e9cc464d3e6a4c7d2
David Quast <dquast@ryerson.ca>
2019-01-17 15:21:22 -0500

    build(): change stencil generated package name

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,7 +2,7 @@ import { Config } from '@stencil/core';
 import { sass } from '@stencil/sass';
 
 export const config: Config = {
-  namespace: 'ryelib',
+  namespace: 'rl-web-components',
   plugins: [
     sass()
   ],
